f0a8843eb6ab01b4cb6093a0dbe8bf0fc668b32f
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2023-02-14 20:21:16 -0600

    Delete src directory

--- src/main/java/com/practiced/rca/security/JwtGeneratorInt.java
+++
@@ -1,9 +0,0 @@
-package com.practiced.rca.security;
-
-import java.io.UnsupportedEncodingException;
-
-import com.practiced.rca.entity.Cliente;
-
-public interface JwtGeneratorInt {
-	public String generateToken(Cliente cliente) throws UnsupportedEncodingException;
-}
